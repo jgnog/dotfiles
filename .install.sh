ec64cfb920f19f7e9bda38166ffbe3fb06664139
Gonçalo Nogueira <jgoncalonogueira@gmail.com>
2016-12-29 14:43:10 +0000

    Removed zsh related stuff from install script.

--- .install.sh
+++ .install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/.dotfilesbk             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim"    # list of files/folders to symlink in homedir
 
 ##########
 
