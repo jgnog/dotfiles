842b87bdd3eb69ef3213de06aeef35c78ff77920
goncalo.nogueira <jgoncalonogueira@gmail.com>
2018-11-30 20:03:23 +0000

    Remove speedswapper from list of files

--- .install.sh
+++ .install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/.dotfilesbk             # old dotfiles backup directory
-files="bashrc vimrc vim bash_aliases speedswapper"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim bash_aliases"    # list of files/folders to symlink in homedir
 
 ##########
 
