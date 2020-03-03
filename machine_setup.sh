73fa0e1329ade34cbe442cc0e54aef921047a1a6
Gonçalo Nogueira <jgoncalonogueira@gmail.com>
2020-03-03 12:08:56 +0000

    Add python3-venv to list of packages to install in machine_setup

--- machine_setup.sh
+++ machine_setup.sh
@@ -42,7 +42,7 @@ cd ~
 sudo apt-get -y install tmux
 
 # Install some utilities
-sudo apt-get -y install htop xclip w3m silversearcher-ag ncdu python3-pip
+sudo apt-get -y install htop xclip w3m silversearcher-ag ncdu python3-pip python3-venv
 
 # Install latex
 sudo apt-get -y install texlive-latex-extra texlive-pictures
