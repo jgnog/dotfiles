c29959869cc63d0e5015db923b16ed30f29e682c
Gonçalo Nogueira <jgoncalonogueira@gmail.com>
2020-11-20 19:46:11 +0000

    Remove some software

--- machine_setup.sh
+++ machine_setup.sh
@@ -12,23 +12,23 @@ sudo apt update && sudo apt upgrade
 sudo apt install --assume-yes $(cat packages_to_install)
 
 # Generate RSA ssh key pair,add it to ssh-agent and to GitHub
-function git_upload_ssh_key () {
-    read -p "Enter github email : " email
-    echo "Using email $email"
-    if [ ! -f ~/.ssh/id_rsa ]; then
-        ssh-keygen -t rsa -b 4096 -C "$email"
-	eval `ssh-agent`
-        ssh-add ~/.ssh/id_rsa
-    fi
-    pub=`cat ~/.ssh/id_rsa.pub`
-    read -p "Enter github username: " githubuser
-    echo "Using username $githubuser"
-    read -s -p "Enter github password for user $githubuser: " githubpass
-    curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"`hostname`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
-}
-
-git_upload_ssh_key
-
+ function git_upload_ssh_key () {
+     read -p "Enter github email : " email
+     echo "Using email $email"
+     if [ ! -f ~/.ssh/id_rsa ]; then
+         ssh-keygen -t rsa -b 4096 -C "$email"
+ 	eval `ssh-agent`
+         ssh-add ~/.ssh/id_rsa
+     fi
+     pub=`cat ~/.ssh/id_rsa.pub`
+     read -p "Enter github username: " githubuser
+     echo "Using username $githubuser"
+     read -s -p "Enter github password for user $githubuser: " githubpass
+     curl -u "$githubuser:$githubpass" -X POST -d "{\"title\":\"`hostname`\",\"key\":\"$pub\"}" https://api.github.com/user/keys
+ }
+ 
+ git_upload_ssh_key
+ 
 # Clone dotfiles repo and install dotfiles
 git clone git@github.com:jgnog/dotfiles.git ~/dotfiles
 # Install Vundle
@@ -45,15 +45,8 @@ cd ~/dotfiles
 make
 cd ~
 
-# Initiate rclone config
-rclone config
-
 # Install Anaconda
 sudo apt-get -y install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
 bash Anaconda3-2019.10-Linux-x86_64.sh
 rm Anaconda3-2019.10-Linux-x86_64.sh
-
-# Install Google Cloud SDK and run the initial configuration
-sudo snap install --classic google-cloud-sdk
-gcloud init
