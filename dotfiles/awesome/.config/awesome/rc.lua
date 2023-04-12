373c02648091ac0861f2c952e2cdf652154bc96b
Gonçalo Nogueira <jgoncalonogueira@gmail.com>
2023-04-12 14:58:03 +0100

    Add hotkey to launch Chromium

--- dotfiles/awesome/.config/awesome/rc.lua
+++ dotfiles/awesome/.config/awesome/rc.lua
@@ -317,6 +317,8 @@ globalkeys = gears.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
+    awful.key({ modkey, "Shift" }, "Return", function () awful.spawn("chromium") end,
+          {description = "Chromium browser", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
