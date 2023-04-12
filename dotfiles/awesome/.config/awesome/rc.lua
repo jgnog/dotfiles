834b55de96cd8875538ba0be5391c59e4f439bb5
Gonçalo Nogueira <jgoncalonogueira@gmail.com>
2023-04-12 15:32:47 +0100

    Add a hotkey for locking the screen

--- dotfiles/awesome/.config/awesome/rc.lua
+++ dotfiles/awesome/.config/awesome/rc.lua
@@ -270,6 +270,8 @@ root.buttons(gears.table.join(
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
+    awful.key({ modkey }, "l", function () awful.spawn("slock") end,
+             {description = "lock screen", group = "awesome"}),
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
