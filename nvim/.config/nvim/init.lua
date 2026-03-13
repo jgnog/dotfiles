4e838f17691c730d9650c27edb5b4aba6985f855
Gonçalo Nogueira <g.nogueira@kaizengaming.com>
2026-03-13 14:31:12 +0000

    Remove complex Bclose function and its keymap
    Use :bd[elete] instead

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -263,74 +263,6 @@ if vim.fn.executable('win32yank.exe') == 1 then
 end
 
 -- =============================================================================
--- Complex Mappings & Legacy Functions (Bclose)
--- =============================================================================
-
--- Wrapping Bclose in vim.cmd as it is complex legacy Vimscript
-vim.cmd [[
-" Delete buffer while keeping window layout
-if v:version < 700 || exists('loaded_bclose') || &cp
-  finish
-endif
-let loaded_bclose = 1
-if !exists('bclose_multiple')
-  let bclose_multiple = 1
-endif
-
-function! s:Warn(msg)
-  echohl ErrorMsg
-  echomsg a:msg
-  echohl NONE
-endfunction
-
-function! s:Bclose(bang, buffer)
-  if empty(a:buffer)
-    let btarget = bufnr('%')
-  elseif a:buffer =~ '^\d\+$'
-    let btarget = bufnr(str2nr(a:buffer))
-  else
-    let btarget = bufnr(a:buffer)
-  endif
-  if btarget < 0
-    call s:Warn('No matching buffer for '.a:buffer)
-    return
-  endif
-  if empty(a:bang) && getbufvar(btarget, '&modified')
-    call s:Warn('No write since last change for buffer '.btarget.' (use :Bclose!)')
-    return
-  endif
-  let wnums = filter(range(1, winnr('$')), 'winbufnr(v:val) == btarget')
-  if !g:bclose_multiple && len(wnums) > 1
-    call s:Warn('Buffer is in multiple windows (use ":let bclose_multiple=1")')
-    return
-  endif
-  let wcurrent = winnr()
-  for w in wnums
-    execute w.'wincmd w'
-    let prevbuf = bufnr('#')
-    if prevbuf > 0 && buflisted(prevbuf) && prevbuf != btarget
-      buffer #
-    else
-      bprevious
-    endif
-    if btarget == bufnr('%')
-      let blisted = filter(range(1, bufnr('$')), 'buflisted(v:val) && v:val != btarget')
-      let bhidden = filter(copy(blisted), 'bufwinnr(v:val) < 0')
-      let bjump = (bhidden + blisted + [-1])[0]
-      if bjump > 0
-        execute 'buffer '.bjump
-      else
-        execute 'enew'.a:bang
-      endif
-    endif
-  endfor
-  execute 'bdelete'.a:bang.' '.btarget
-  execute wcurrent.'wincmd w'
-endfunction
-command! -bang -complete=buffer -nargs=? Bclose call <SID>Bclose(<q-bang>, <q-args>)
-]]
-
--- =============================================================================
 -- Key Mappings
 -- =============================================================================
 
@@ -338,14 +270,11 @@ command! -bang -complete=buffer -nargs=? Bclose call <SID>Bclose(<q-bang>, <q-ar
 local map = vim.keymap.set
 local opts = { silent = true }
 
--- Bclose mapping
-map('n', '<leader>q', ':Bclose<CR>', opts)
-
 -- Avoid escape
 map('i', 'jk', '<Esc>')
 
 -- Clear highlighting on double <esc> press
-map('n', '<esc><esc>', ':noh<return><esc>', opts)
+map('n', '<esc><esc>', '<cmd>nohlsearch<CR>', opts)
 
 -- Treat longlines as different lines
 map('n', 'j', 'gj')
