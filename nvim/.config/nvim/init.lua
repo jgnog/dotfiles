72dad81848d9008ed3b7769bef9b70cbec37395c
Gonçalo Nogueira <g.nogueira@kaizengaming.com>
2026-03-20 16:22:17 +0000

    Do not clutter window with diagnostics

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -229,10 +229,10 @@ vim.diagnostic.config({
 	update_in_insert = false,
 	severity_sort = true,
 	float = { border = "rounded", source = "if_many" },
-	underline = { severity = { min = vim.diagnostic.severity.WARN } },
+	underline = false,
 
 	-- Can switch between these as you prefer
-	virtual_text = true, -- Text shows up at the end of the line
+	virtual_text = false, -- Text shows up at the end of the line
 	virtual_lines = false, -- Text shows up underneath the line, with virtual lines
 
 	-- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
